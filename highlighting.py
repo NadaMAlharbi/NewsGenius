3fafcf74f5881ea9802526da669b7f40bb79b8eb
Nada Alharbi <alharbi13nada@gmail.com>
2024-10-23 11:12:45 +0300

    Update highlighting.py

--- highlighting.py
+++ highlighting.py
@@ -4,8 +4,8 @@ def normalize_sentence(sentence):
 
 # Function to provide a predefined set of distinct pastel colors
 def generate_distinct_colors(n):
-    # Fixed list of colors (LemonChiffon, MintCream, LightCyan, MistyRose, PeachPuff)
-    colors = ['#FFFACD', '#D3F8E2', '#D4F1F4', '#FFE4E1', '#FFDAB9']
+    # Fixed list of colors (LemonChiffon, MintCream, LightCyan, MistyRose, PeachPuff, LavenderBlush, Honeydew)
+    colors = ['#FFFACD', '#D3F8E2', '#D4F1F4', '#FFE4E1', '#FFDAB9', '#FFF0F5', '#F0FFF0']
     # Return enough colors for `n` sentences by cycling through the list if necessary
     return [colors[i % len(colors)] for i in range(n)]
 
